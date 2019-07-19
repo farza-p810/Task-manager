94438a57aef4cadf972c7a12f0f72ed1798be0c8
Farzaneh Parvar <farzane.parvar@gmail.com>
2019-07-19 14:26:25 +0430

    Complete task edit

--- application/controllers/TaskController.js
+++ application/controllers/TaskController.js
@@ -37,12 +37,64 @@ exports.store = (req, res) => {
   });
 };
 
+exports.show = (req, res) => {
+  const id = req.params.id;
+  db.query(
+    "SELECT `id`, `title`, `category`, `assignee`, `status` FROM `tasks` WHERE id = ? LIMIT 1",
+    [id],
+    (err, tasks) => {
+      if (err) throw err;
+      const data = {
+        task: tasks[0],
+        taskStatus
+      };
+      res.render("task/show", data);
+    }
+  );
+};
+
+exports.edit = (req, res) => {
+  const id = req.params.id;
+  db.query(
+    "SELECT `id`, `title`, `category`, `assignee`, `status` FROM `tasks` WHERE `id` = ? LIMIT 1",
+    id,
+    (err, tasks) => {
+      if (err) throw err;
+      const data = {
+        task: tasks[0],
+        taskStatus
+      };
+      res.render("task/edit", data);
+    }
+  );
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+  const formData = fields(req);
+  db.query(
+    "UPDATE `tasks` SET ? WHERE id = ?",
+    [formData, id],
+    (err, result) => {
+      if (err) throw err;
+
+      cookieUtitlity.set(res, "message", {
+        type: "success",
+        text: "وظیفه موردنظر با موفقیت ویرایش شد."
+      });
+
+      return res.redirect("/tasks");
+    }
+  );
+};
+
 const fields = req => {
-  const { title, category, assignee } = req.body;
+  const { title, category, assignee, status } = req.body;
+
   return {
     title,
     category,
     assignee,
-    status: taskStatus.CREATED
+    status: status || taskStatus.CREATED
   };
 };
--- routes/task.js
+++ routes/task.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const {
   index,
   create,
-  store
+  store,
+  show,
+  edit,
+  update
 } = require("../application/controllers/TaskController");
 
 router.get("/", index);
+router.get("/show/:id", show);
 router.get("/create", create);
 router.post("/store", store);
+router.get("/edit/:id", edit);
+router.post("/update/:id", update);
 
 module.exports = router;
